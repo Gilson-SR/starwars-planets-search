c0906cf00515ab01551f0dc8d22bf0fc8f1b1c0c
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-10-22 20:46:22 -0300

    Testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+import dataApi from './Mock/dataApi'
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe("", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(dataApi),
+      })
+    );
+  });
+
+  test("", async () => {
+    render(<App />);
+    expect(fetch).toHaveBeenCalled();
+    const filterName = screen.getByTestId("name-filter");
+    expect(filterName).toBeInTheDocument();
+    userEvent.type(filterName, "oo");
+    expect(filterName).toHaveValue("oo");
+    const planets = await screen.findAllByTestId("planet-name");
+    expect(planets).toHaveLength(2);
+  });
 });
